006c7611e643c0de6a8716f76ea50936939707e1
Douglas <wdmdavison@icloud.com>
2024-02-20 13:32:31 -0600

    change switch to if

--- 0x00-hello-dart/6-positive_or_negative.dart
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -1,11 +1,10 @@
 void main(List<String> args) {
     var number = int.parse(args[0]);
     var statement = " is zero";
-    switch (number) {
-        case number > 0:
-            statement = " is positive";
-        case number < 0:
-            statement = " is negative";
+    if (number > 0) {
+        statement = " is positive";
+    } else if (number < 0) {
+        statement = " is negative";
     }
     print('$number$statement');
 }
