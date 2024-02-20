f9410a2ddc2f63e4c2724148dc23a441da6f9641
Douglas <wdmdavison@icloud.com>
2024-02-20 13:18:33 -0600

    move parentheses in assert

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -3,6 +3,6 @@ void main(List<String> args) {
   Write Your code below
    */
   var nb = args[0];
-  assert(int.parse(nb) >= 80),
-  'Uncaught Error: Assertion failed: "The score must be bigger or equal to 80';
+  assert(int.parse(nb) >= 80,
+  'Uncaught Error: Assertion failed: "The score must be bigger or equal to 80');
 }
