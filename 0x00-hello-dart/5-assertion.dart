ae9ed5d2d2ff37c537f39165e6742a2ddb0b8795
Douglas <wdmdavison@icloud.com>
2024-02-20 13:19:33 -0600

    add message on success

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -5,4 +5,5 @@ void main(List<String> args) {
   var nb = args[0];
   assert(int.parse(nb) >= 80,
   'Uncaught Error: Assertion failed: "The score must be bigger or equal to 80');
+  print('You Passed');
 }
