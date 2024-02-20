dbc88b9babeb227d7d6c3168fc5af0eb9bc4d430
Douglas <wdmdavison@icloud.com>
2024-02-20 13:17:34 -0600

    push 5 for testing

---
+++ 0x00-hello-dart/5-assertion.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  /*
+  Write Your code below
+   */
+  var nb = args[0];
+  assert(int.parse(nb) >= 80),
+  'Uncaught Error: Assertion failed: "The score must be bigger or equal to 80';
+}
