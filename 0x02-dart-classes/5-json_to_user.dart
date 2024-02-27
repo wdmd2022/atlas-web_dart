1780ec8be3364c5e5640f6213adf49dda45f23f4
Douglas <wdmdavison@icloud.com>
2024-02-27 03:28:24 -0600

    fix idiosyncratic spacing on 5

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -25,6 +25,6 @@ class User {
   }
 
   String toString() {
-     return 'User(id : $id, name: $name, age: $age, height: $height)';
+     return 'User(id : $id, name: $name ,age: $age ,height: $height)';
   }
 }
