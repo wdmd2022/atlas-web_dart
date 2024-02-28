2734c4c6747b2eab4ab7db43584990c665ec49bc
Douglas <wdmdavison@icloud.com>
2024-02-27 21:14:49 -0600

    simulate api call

---
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: unused_import
+import 'dart:convert';
+import 'dart:core';
+import 'package:http/http.dart' as http;
+
+Future<void> printRmCharacteer async {
+    final chars = [
+        "Rick Sanchez",
+        "Morty Smith",
+        "Summer Smith",
+        "Beth Smith",
+        "Jerry Smith",
+        "Abadango Cluster Princess",
+        "Abradolf Lincler",
+        "Adjudicator Rick",
+        "Agency Director",
+        "Alan Rails",
+        "Albert Einstein",
+        "Alexander",
+        "Alien Googah",
+        "Alien Morty",
+        "Alien Rick",
+        "Amish Cyborg",
+        "Annie",
+        "Antenna Morty",
+        "Antenna Rick",
+        "Ants in my Eyes Johnson",
+    ];
+    for (var char in chars) {
+        print(char);
+    }
+}
