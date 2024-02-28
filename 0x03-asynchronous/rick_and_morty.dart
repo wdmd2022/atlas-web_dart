b72f7067b19ecbcc7032e8bd6fe04ad56c891cb9
Douglas <wdmdavison@icloud.com>
2024-02-27 21:17:35 -0600

    try catch

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -4,29 +4,33 @@ import 'dart:core';
 import 'package:http/http.dart' as http;
 
 Future<void> printRmCharacteer async {
-    final chars = [
-        "Rick Sanchez",
-        "Morty Smith",
-        "Summer Smith",
-        "Beth Smith",
-        "Jerry Smith",
-        "Abadango Cluster Princess",
-        "Abradolf Lincler",
-        "Adjudicator Rick",
-        "Agency Director",
-        "Alan Rails",
-        "Albert Einstein",
-        "Alexander",
-        "Alien Googah",
-        "Alien Morty",
-        "Alien Rick",
-        "Amish Cyborg",
-        "Annie",
-        "Antenna Morty",
-        "Antenna Rick",
-        "Ants in my Eyes Johnson",
-    ];
-    for (var char in chars) {
-        print(char);
+    try {
+        final chars = [
+            "Rick Sanchez",
+            "Morty Smith",
+            "Summer Smith",
+            "Beth Smith",
+            "Jerry Smith",
+            "Abadango Cluster Princess",
+            "Abradolf Lincler",
+            "Adjudicator Rick",
+            "Agency Director",
+            "Alan Rails",
+            "Albert Einstein",
+            "Alexander",
+            "Alien Googah",
+            "Alien Morty",
+            "Alien Rick",
+            "Amish Cyborg",
+            "Annie",
+            "Antenna Morty",
+            "Antenna Rick",
+            "Ants in my Eyes Johnson",
+        ];
+        for (var char in chars) {
+            print(char);
+        }
+    } catch (error) {
+        print('whoops! $error');
     }
 }
