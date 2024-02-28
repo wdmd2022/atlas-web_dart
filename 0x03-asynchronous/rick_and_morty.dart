050e29c615aab3c6edbb2d58c0f1199b84f9d52f
Douglas <wdmdavison@icloud.com>
2024-02-27 21:35:04 -0600

    remove print

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -9,8 +9,8 @@ Future<void> printRmCharacters() async {
         // get the content there
         var response = await http.get(url);
         // let's get loopy
-        for (Map chars in jsonDecode(response.body)["results"]) {
-            print(chars["name"]);
+        // for (Map chars in jsonDecode(response.body)["results"]) {
+        //     print(chars["name"]);
         }
     }   catch (error) {
             print("error caught: ${error}");
